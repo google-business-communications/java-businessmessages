ed4dcb1ed70fc133850bea4562d513f1161708d5
Adam Chan <adamjchan@google.com>
2021-01-14 21:50:29 +0000

    Updated to enable image messages

--- com/google/api/services/businessmessages/v1/model/BusinessMessagesImage.java
+++ com/google/api/services/businessmessages/v1/model/BusinessMessagesImage.java
@@ -19,7 +19,7 @@
 package com.google.api.services.businessmessages.v1.model;
 
 /**
- * Standalone card.
+ * An image.
  *
  * <p> This is the Java data model class that specifies how to parse/serialize into the JSON that is
  * transmitted over HTTP when working with the Business Messages API. For a detailed explanation
@@ -30,40 +30,43 @@ package com.google.api.services.businessmessages.v1.model;
  * @author Google, Inc.
  */
 @SuppressWarnings("javadoc")
-public final class BusinessMessagesStandaloneCard extends com.google.api.client.json.GenericJson {
+public final class BusinessMessagesImage extends com.google.api.client.json.GenericJson {
 
   /**
-   * Card content.
+   * Information about an image, including the URL of the image and the URL of the image's
+   * thumbnail.
    * The value may be {@code null}.
    */
   @com.google.api.client.util.Key
-  private BusinessMessagesCardContent cardContent;
+  private BusinessMessagesContentInfo contentInfo;
 
   /**
-   * Card content.
+   * Information about an image, including the URL of the image and the URL of the image's
+   * thumbnail.
    * @return value or {@code null} for none
    */
-  public BusinessMessagesCardContent getCardContent() {
-    return cardContent;
+  public BusinessMessagesContentInfo getContentInfo() {
+    return contentInfo;
   }
 
   /**
-   * Card content.
-   * @param cardContent cardContent or {@code null} for none
+   * Information about an image, including the URL of the image and the URL of the image's
+   * thumbnail.
+   * @param contentInfo contentInfo or {@code null} for none
    */
-  public BusinessMessagesStandaloneCard setCardContent(BusinessMessagesCardContent cardContent) {
-    this.cardContent = cardContent;
+  public BusinessMessagesImage setContentInfo(BusinessMessagesContentInfo contentInfo) {
+    this.contentInfo = contentInfo;
     return this;
   }
 
   @Override
-  public BusinessMessagesStandaloneCard set(String fieldName, Object value) {
-    return (BusinessMessagesStandaloneCard) super.set(fieldName, value);
+  public BusinessMessagesImage set(String fieldName, Object value) {
+    return (BusinessMessagesImage) super.set(fieldName, value);
   }
 
   @Override
-  public BusinessMessagesStandaloneCard clone() {
-    return (BusinessMessagesStandaloneCard) super.clone();
+  public BusinessMessagesImage clone() {
+    return (BusinessMessagesImage) super.clone();
   }
 
 }
